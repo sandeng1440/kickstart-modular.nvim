0e80cd84edc23680e3b252010e0325a8e22cb5fa
sandeng1440 <sandeng1440@gmail.com>
2024-09-21 12:16:05 +0300

    Added catpuccin

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+local gruvbox = {
+  'ellisonleao/gruvbox.nvim',
+  priority = 1000,
+  config = true,
+  opts = ...,
+  init = function()
+    vim.cmd 'colorscheme gruvbox'
+  end,
+}
+
+local tokyonight = { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
+}
+
+local catppuccin = {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  init = function()
+    require('catppuccin').setup {
+      flavour = 'mocha',
+    }
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
+
+return catppuccin
