df6b5b81d2157241e180ff0c46e52b25295ad6ba
Santiago Deng <sandeng1440@gmail.com>
2025-04-08 18:05:02 +0300

    updates

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -7,7 +7,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end ---@diagnostic disable-next-line: undefined-field
+end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- vim: ts=2 sts=2 sw=2 et
